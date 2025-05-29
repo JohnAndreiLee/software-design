4732036587f5851704475d396cbcdbd31d8652c5
John Andrei Lee <67588993+jandreeeh@users.noreply.github.com>
2025-05-29 14:58:43 +0800

    a

--- lab4/Program.cs
+++ lab4/Program.cs
@@ -1,4 +1,6 @@
+
 namespace lab4
+
 {
 	internal static class Program
 	{
@@ -6,15 +8,12 @@ namespace lab4
 		///  The main entry point for the application.
 		/// </summary>
 		[STAThread]
-		static void Main()
+        static void Main()
 		{
-			Test test = new Test();
-			test.TestTextBookCreation();
-			test.TestMagazineCreation();
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            //ApplicationConfiguration.Initialize();
-            //Application.Run(new Form1());
-        }
+			// To customize application configuration such as set high DPI settings or default font,
+			// see https://aka.ms/applicationconfiguration.
+			ApplicationConfiguration.Initialize();
+			Application.Run(new Form1());
+		}
 	}
 }
