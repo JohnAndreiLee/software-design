69aa1b436de9f503ce10898d3d492f96517ec167
John Andrei Lee <67588993+jandreeeh@users.noreply.github.com>
2025-05-29 18:19:05 +0800

    started week 5

--- lab5/Form1.cs
+++ lab5/Form1.cs
@@ -5,6 +5,23 @@ namespace lab5
 		public Form1()
 		{
 			InitializeComponent();
-		}
-	}
+        }
+        
+    }
+
+    public class Author
+    {
+        public int AuthorID { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
+    }
+
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public int AuthorID { get; set; }
+        public virtual Author Author { get; set; }
+    }
+}
 }
